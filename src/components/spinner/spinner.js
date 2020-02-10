27c648886ac1084e99493345061b96502f1aa6a0
Elvin Samanov <samanov.elvin96@mail.ru>
2020-02-10 14:10:08 +0400

    first commit

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+  return <div><div className="loadingio-spinner-rolling-wgbu15r3gu"><div class="ldio-8014ejw1jwu">
+  <div></div>
+  </div></div></div>;
+};
+
+export default Spinner;
+
